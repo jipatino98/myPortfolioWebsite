8716b886c7fa82d7c37bfc3fd258c9e540b9070c
Jose Patino <120534408+jipatino98@users.noreply.github.com>
2024-01-24 16:48:57 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Jose Patino Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
